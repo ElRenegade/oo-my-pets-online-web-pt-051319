1076969f0ffe4455c9b916bde1ae85c38359496c
Santiago Sanchez <chagostwo@yahoo.com>
2019-06-14 03:50:53 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,7 +1,7 @@
 class Owner
   attr_reader :species
   attr_accessor :name, :pets, :fish, :dog, :cat
-  @@pets = {fishes:[], dogs:[], cats:[]}
+  @pets = {fishes:[], dogs:[], cats:[]}
   @@all = []
 
   def initialize(species)
@@ -27,44 +27,41 @@ class Owner
   end
 
   def pets
-    @@pets
+    @pets
   end
 
   def buy_fish(fish)
-    @@pets[:fishes] << Fish.new(fish)
+    @pets[:fishes] << Fish.new(fish)
   end
 
   def buy_cat(cat)
-    @@pets[:cats] << Cat.new(cat)
+    @pets[:cats] << Cat.new(cat)
   end
 
   def buy_dog(dog)
-    @@pets[:dogs] << Dog.new(dog)
+    @pets[:dogs] << Dog.new(dog)
   end
 
   def walk_dogs
-    @@pets[:dogs].each{|dog|
+    @pets[:dogs].each{|dog|
     dog.mood = "happy"}
   end
 
   def play_with_cats
-    @@pets[:cats].each{|cat|
+    @pets[:cats].each{|cat|
     cat.mood = "happy"}
   end
 
   def feed_fish
-    @@pets[:fishes].each{|fish|
+    @pets[:fishes].each{|fish|
     fish.mood = "happy"}
   end
 
 
   def sell_pets
-    pets.each do |pet, arr|
-      arr.map do |pet|
-        pet.mood = 'nervous'
+    pets.each do |type, pets|
+      pets.map do |pet| pet.mood = "nervous"
       end
-      arr.clear
-    end
   end
 
   def list_pets
