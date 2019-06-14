000d6f3f2a74708655c1f29a9ecc006e4c52b0c4
Santiago Sanchez <chagostwo@yahoo.com>
2019-06-14 03:59:06 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -31,15 +31,18 @@ class Owner
   end
 
   def buy_fish(fish)
-    @@pets[:fishes] << Fish.new(fish)
+    fish = Fish.new(name)
+    @@pets[:fishes] << fish
   end
 
   def buy_cat(cat)
-    @@pets[:cats] << Cat.new(cat)
+    cat = Cat.new(name)
+    @@pets[:cats] << cat
   end
 
   def buy_dog(dog)
-    @@pets[:dogs] << Dog.new(dog)
+    dog = Dog.new(name)
+    @@pets[:dogs] << dog
   end
 
   def walk_dogs
